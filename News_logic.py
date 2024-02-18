f95636395e8d0974a4b806da46fd1240fc5b8dfa
Abhaykv <abhaykv111@gmail.com>
2024-02-18 12:28:17 +0530

    Adding News Article Retrival Logic and GPT Query Logic

---
+++ News_logic.py
@@ -0,0 +1,51 @@
+from newsdataapi import NewsDataApiClient
+
+class News:
+    """
+    A class to interact with a news API and retrieve articles based on specific criteria.
+
+    Attributes:
+        API_KEY (str): The API key used to authenticate with the news API.
+        api (NewsDataApiClient): An instance of the NewsDataApiClient class for making API requests.
+    """
+
+    def __init__(self, API_KEY):
+        """
+        Initializes the News class with the provided API key.
+
+        Args:
+            API_KEY (str): The API key used to authenticate with the news API.
+        """
+        self.api = NewsDataApiClient(apikey=API_KEY)
+
+    def get_articles(self, query, country, lan ):
+        """
+        Retrieves articles from the news API based on the provided query, country, and language.
+
+        Args:
+            query (str): The search query used to filter articles.
+            country (str): The country code (ISO 3166-1 alpha-2) for filtering articles by country.
+            lan (str): The language code (ISO 639-1) for filtering articles by language.
+
+        Returns:
+            str: A formatted string containing information about the retrieved articles.
+        """
+        # Retrieve articles from the news API
+        response = self.api.news_api(q=query, country=country, language=lan)
+        result_string = ""
+        articles = response.get('results', [])
+
+        # Extract relevant information from each article
+        article_info = [(art.get('title', ''), art.get('description', ''), art.get('content', ''), art.get('link', '')) for art in articles]
+        
+        # Format the retrieved article information into a string
+        for title, description, content, link in article_info:
+            result_string += f"Title: {title}\nDescription: {description}\nContent: {content}\nLink: {link}\n\n"
+
+        return result_string
+
+# # Instantiate the News class with the API key
+# news = News(API_KEY=API_KEY)
+
+# # Retrieve and print articles based on the specified query, country, and language
+# print(news.get_articles(query = "ronaldo", country = "us", lan = "en"))
