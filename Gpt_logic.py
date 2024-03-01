12e04972154c9795e187043a3dadd3d422f9c509
Abhaykv <abhaykv111@gmail.com>
2024-03-01 21:46:10 +0530

    added simplifications

--- Gpt_logic.py
+++ Gpt_logic.py
@@ -1,5 +1,4 @@
 from openai import OpenAI
-import os
 
 class GP_Generate:
     """
@@ -27,31 +26,30 @@ class GP_Generate:
             str: The generated text containing answers to user queries.
         """
         # Create a completion request
-        history = "" 
         while(True):
-            message = input("\nEnter STOP to stop the program \n continous running will cause credit to decrease \nEnter Your Query: \n ")
-            if message != "STOP":
-                history =  history+"\n new query: "+message
-                completion = self.client.chat.completions.create(
-                    model="gpt-3.5-turbo",  # Specify the GPT model
-                    messages=[
-                        {"role": "system", "content": history},
-                    ]
-                )
-                # Return the generated text
-                history += "\n Answer: "+completion.choices[0].message.content + "\n"
-            else:
+            message = input("\n Enter STOP to stop the program \n continous running will cause credit to decrease \nEnter Your Query: \n ")
+            if message == "STOP":
                 break
+            query =  "\n new query: " + message
+            completion = self.client.chat.completions.create(
+                model="gpt-3.5-turbo",  # Specify the GPT model
+                messages=[
+                    {"role": "system", "content": query},
+                ]
+            )
+            # Return the generated text
+            query += "\n Answer: "+completion.choices[0].message.content + "\n"
             
-        return history
+        return query
 
 # Retrieve the API key from environment variables
-api_key = os.getenv("CG_KEY")
+# api_key = "API KEY"
+import os
+# # Create an instance of the Generate class
+generator = GP_Generate(os.getenv("CG_KEY"))
 
-# Create an instance of the Generate class
-generator = GP_Generate(api_key)
-
-# Generate text based on the provided message
+# # Generate text based on the provided message
 
 result = generator.ask()
 print(result)
+# process this result variable for your use cases
